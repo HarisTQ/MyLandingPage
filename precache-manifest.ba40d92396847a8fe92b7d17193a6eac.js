a2ec4965a77ecd604c688936f2937f884822313d
HarisTQ <haristauqeer525@gmail.com>
2025-09-10 13:00:55 +0500

    Updates

---
+++ precache-manifest.ba40d92396847a8fe92b7d17193a6eac.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04a21937389415c24b0a306afccef553",
+    "url": "/MyLandingPage/index.html"
+  },
+  {
+    "revision": "70fe7cad91fd965ee8e9",
+    "url": "/MyLandingPage/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "c22e04e08d4a234c62f9",
+    "url": "/MyLandingPage/static/js/2.57a7f154.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/MyLandingPage/static/js/2.57a7f154.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1b7147dbcfab13b2ea0",
+    "url": "/MyLandingPage/static/js/3.be929129.chunk.js"
+  },
+  {
+    "revision": "70fe7cad91fd965ee8e9",
+    "url": "/MyLandingPage/static/js/main.acf98435.chunk.js"
+  },
+  {
+    "revision": "efb988d275a0bdb13e94",
+    "url": "/MyLandingPage/static/js/runtime-main.ff247836.js"
+  },
+  {
+    "revision": "79ba2002d56019901132986d5c84067a",
+    "url": "/MyLandingPage/static/media/profile.79ba2002.jpg"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "/MyLandingPage/static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "/MyLandingPage/static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "397c455a49d4f872c292a3dc25432579",
+    "url": "/MyLandingPage/static/media/svg-3.397c455a.svg"
+  }
+]);
